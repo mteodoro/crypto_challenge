80e234698b2cde4b9af379b74932a1abb35f41dd
Mark Teodoro <mteodoro@verisign.com>
2013-04-20 10:16:30 -0700

    problem 5

--- crypto1-8.py
+++ crypto1-8.py
@@ -13,7 +13,6 @@ def xor_data(key, data):
     return ''.join(chr(ord(x) ^ ord(y)) for x,y in itertools.izip(data, stream))
 
 
-
 ok = set(string.letters + ' ')
 def score_ratio(s):
     lcount = sum(1 for x in s if x in ok)
@@ -124,8 +123,29 @@ has been encrypted by single-character XOR. Find it. (Your code from
 
 
 def cc5():
-    """"""
-    pass
+    """5. Repeating-key XOR Cipher
+
+Write the code to encrypt the string:
+
+ Burning 'em, if you ain't quick and nimble
+ I go crazy when I hear a cymbal
+
+Under the key "ICE", using repeating-key XOR. It should come out to:
+
+ 0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f
+
+Encrypt a bunch of stuff using your repeating-key XOR function. Get a
+feel for it.
+"""
+    output = '0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f'
+
+    plain = """Burning 'em, if you ain't quick and nimble
+I go crazy when I hear a cymbal"""
+
+    cipher = xor_data('ICE', plain).encode('hex')
+    print '%s...' % cipher[:64]
+    print cipher == output
+
 
 def cc6():
     """"""
