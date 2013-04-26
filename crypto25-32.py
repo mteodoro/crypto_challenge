073ba95a1eb59789c4f0157f66f55df707b22dd9
Mark Teodoro <mteodoro@verisign.com>
2013-04-26 16:20:41 -0700

    problem 28

--- crypto25-32.py
+++ crypto25-32.py
@@ -44,6 +44,88 @@ def pkcs7_strip(data):
     return data[:-padlen]
 
 
+#https://github.com/ajalt/python-sha1
+def sha1(message):
+    """SHA-1 Hashing Function
+
+    A custom SHA-1 hashing function implemented entirely in Python.
+
+    Arguments:
+        message: The input message string to hash.
+
+    Returns:
+        A hex SHA-1 digest of the input message.
+    """
+
+    def _left_rotate(n, b):
+        return ((n << b) | (n >> (32 - b))) & 0xffffffff
+
+    # Initialize variables:
+    h0 = 0x67452301
+    h1 = 0xEFCDAB89
+    h2 = 0x98BADCFE
+    h3 = 0x10325476
+    h4 = 0xC3D2E1F0
+
+    # Pre-processing:
+    original_byte_len = len(message)
+    original_bit_len = original_byte_len * 8
+    # append the bit '1' to the message
+    message += '\x80'
+
+    # append 0 <= k < 512 bits '0', so that the resulting message length (in bits)
+    #    is congruent to 448 (mod 512)
+    message += '\x00' * ((56 - (original_byte_len + 1) % 64) % 64)
+
+    # append length of message (before pre-processing), in bits, as 64-bit big-endian integer
+    message += struct.pack('>Q', original_bit_len)
+    # Process the message in successive 512-bit chunks:
+    # break message into 512-bit chunks
+    for i in xrange(0, len(message), 64):
+        w = [0] * 80
+        # break chunk into sixteen 32-bit big-endian words w[i]
+        for j in xrange(16):
+            w[j] = struct.unpack('>I', message[i + j*4:i + j*4 + 4])[0]
+        # Extend the sixteen 32-bit words into eighty 32-bit words:
+        for j in xrange(16, 80):
+            w[j] = _left_rotate(w[j-3] ^ w[j-8] ^ w[j-14] ^ w[j-16], 1)
+
+        # Initialize hash value for this chunk:
+        a = h0
+        b = h1
+        c = h2
+        d = h3
+        e = h4
+
+        for i in xrange(80):
+            if 0 <= i <= 19:
+                # Use alternative 1 for f from FIPS PB 180-1 to avoid ~
+                f = d ^ (b & (c ^ d))
+                k = 0x5A827999
+            elif 20 <= i <= 39:
+                f = b ^ c ^ d
+                k = 0x6ED9EBA1
+            elif 40 <= i <= 59:
+                f = (b & c) | (b & d) | (c & d)
+                k = 0x8F1BBCDC
+            elif 60 <= i <= 79:
+                f = b ^ c ^ d
+                k = 0xCA62C1D6
+
+            a, b, c, d, e = ((_left_rotate(a, 5) + f + e + k + w[i]) & 0xffffffff,
+                            a, _left_rotate(b, 30), c, d)
+
+        # sAdd this chunk's hash to result so far:
+        h0 = (h0 + a) & 0xffffffff
+        h1 = (h1 + b) & 0xffffffff
+        h2 = (h2 + c) & 0xffffffff
+        h3 = (h3 + d) & 0xffffffff
+        h4 = (h4 + e) & 0xffffffff
+
+    # Produce the final hash value (big-endian):
+    return '%08x%08x%08x%08x%08x' % (h0, h1, h2, h3, h4)
+
+
 def cc25():
     """25. Break "random access read/write" AES CTR
 
@@ -212,6 +294,18 @@ Verify that you cannot tamper with the message without breaking the
 MAC you've produced, and that you can't produce a new MAC without
 knowing the secret key.
 """
+    def authenticate(key, mac, message):
+        return sha1(key + message) == mac
+
+    tests = [
+        ('Original:', 'YELLOW SUBMARINE', "My posse's to the side yellin', Go Vanilla Go!"),
+        ('Bad Key: ', 'ORANGE SUBMARINE', "My posse's to the side yellin', Go Vanilla Go!"),
+        ('Bad Msg: ', 'YELLOW SUBMARINE', "My posse's to the side yellin', Stop Vanilla Stop!")]
+
+    comment, key, msg = tests[0]
+    mac = sha1(key + msg)
+    for comment, key, msg in tests:
+        print '%s %s %s\t%s' % (comment, key, msg, authenticate(key, mac, msg))
 
 
 def cc29():
