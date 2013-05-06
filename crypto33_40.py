bbc65006d538839d043e99d1bfa3c0a3f952cac0
Mark Teodoro <mteodoro@verisign.com>
2013-05-06 10:55:05 -0700

    problem 33

--- crypto33_40.py
+++ crypto33_40.py
@@ -1,4 +1,19 @@
 #!/usr/bin/env python
+import sys
+from hashlib import sha256
+import random
+import itertools
+
+random.seed('matasano') #for reproducibility - will work with any seed
+
+
+#modified from http://docs.python.org/2/library/itertools.html#recipes
+def grouper(n, iterable, fillvalue=None):
+    "Collect data into fixed-length chunks or blocks"
+    # grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx
+    args = [iter(iterable)] * n
+    groups = itertools.izip_longest(fillvalue=fillvalue, *args)
+    return (''.join(group) for group in groups)
 
 
 def cc33():
@@ -48,6 +63,38 @@ math, don't freak out), because you'll blow out your bignum library
 raising "a" to the 1024-bit-numberth power. You can find modexp
 routines on Rosetta Code for most languages.
 """
+    def make_keys(p, g):
+        x = random.randint(0, sys.maxint) % p
+        return x, pow(g, x, p) #(g**x) % p
+
+    #p, g = 37, 5
+    p = int(''.join("""
+ffffffffffffffffc90fdaa22168c234c4c6628b80dc1cd129024
+e088a67cc74020bbea63b139b22514a08798e3404ddef9519b3cd
+3a431b302b0a6df25f14374fe1356d6d51c245e485b576625e7ec
+6f44c42e9a637ed6b0bff5cb6f406b7edee386bfb5a899fa5ae9f
+24117c4b1fe649286651ece45b3dc2007cb8a163bf0598da48361
+c55d39a69163fa8fd24cf5f83655d23dca3ad961c62f356208552
+bb9ed529077096966d670c354e4abc9804f1746c08ca237327fff
+fffffffffffff""".strip().split()), 16)
+    g = 2
+    print 'p:', p
+    print 'g:', g
+    print
+
+    a, A = make_keys(p, g)
+    b, B = make_keys(p, g)
+
+    s1 = pow(B, a, p)
+    s2 = pow(A, b, p)
+    print 's1:', s1
+    print 's2:', s2
+    print 's1 == s2:', s1 == s2
+    print
+
+    s1key, s1mac = grouper(32, sha256(hex(s1)[2:]).hexdigest())
+    print 'key:', s1key, 'mac:', s1mac
+
 
 
 def cc34():
