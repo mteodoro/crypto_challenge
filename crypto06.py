d8aed6b5af49c64bcea2e1a1fa5c82c1ab836576
Mark Teodoro <mteodoro@verisign.com>
2013-05-13 22:29:46 -0700

    started problem 43

--- crypto06.py
+++ crypto06.py
@@ -63,6 +63,25 @@ def rsa_genkeys(bits, e):
     return (e,n), (d,n)
 
 
+dsa_p = long(''.join("""
+800000000000000089e1855218a0e7dac38136ffafa72eda7
+859f2171e25e65eac698c1702578b07dc2a1076da241c76c6
+2d374d8389ea5aeffd3226a0530cc565f3bf6b50929139ebe
+ac04f48c3c84afb796d61e5a4f9a8fda812ab59494232c7d2
+b4deb50aa18ee9e132bfa85ac4374d7f9091abc3d015efc87
+1a584471bb1""".strip().split()), 16)
+
+dsa_q = long('f4f47f05794b256174bba6e9b396a7707e563c5b', 16)
+
+dsa_g = long(''.join("""
+5958c9d3898b224b12672c0b98e06c60df923cb8bc999d119
+458fef538b8fa4046c8db53039db620c094c9fa077ef389b5
+322a559946a71903f990f1f7e0e025e2d7f7cf494aff1a047
+0f5b64c36b625a097f1651fe775323556fe00b3608c887892
+878480e99041be601a62166ca6894bdd41a7054ec89f756ba
+9fc95302291""".strip().split()), 16)
+
+
 def cc41():
     """41. Implement Unpadded Message Recovery Oracle
 
@@ -326,6 +345,12 @@ Its SHA-1 fingerprint (after being converted to hex) is:
 
 Obviously, it also generates the same signature for that string.
 """
+    p, q, g = dsa_p, dsa_q, dsa_g
+
+    for k in xrange(1, 2**16 + 1):
+        r = pow(g, k, p) % q
+        if r == 548099063082341131477253921760299949438196259240:
+            print k
 
 
 def cc44():
